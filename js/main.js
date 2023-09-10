964487ab14855be21c15d2ff440d8b510a103ee6
marwanhomran <marwanhom9@outlook.com>
2023-09-10 17:41:29 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//filter Js
+$(document).ready(function(){
+    $('.filter-item').click(function(){
+        const value = $(this).attr('data-filter')
+        if (value == 'all') {
+            $('.post-box').show('1000')
+        }
+        else {
+            $('post-box').not('.' + value).hide('1000')
+            $('post-box').filter('.' + value).show('1000')
+        }
+    })
+})
