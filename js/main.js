9ec7a1efe53256c949b86ccf7d8270d9ee863cf7
marwanhomran <marwanhom9@outlook.com>
2023-09-14 02:36:25 +0300

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-//filter Js
-$(document).ready(function(){
-    $('.filter-item').click(function(){
-        const value = $(this).attr('data-filter')
-        if (value == 'all') {
-            $('.post-box').show('1000')
-        }
-        else {
-            $('post-box').not('.' + value).hide('1000')
-            $('post-box').filter('.' + value).show('1000')
-        }
-    })
-})
+
+
+// show login password function 
+function showPassword() {
+
+    let passwprd = document.getElementById('loginPassword')
+    if (passwprd.type === 'password') {
+        passwprd.type = 'text';
+    } else {
+       passwprd.type = 'password';
+    }
+        
+};
